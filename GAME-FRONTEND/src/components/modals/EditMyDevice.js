855ca50ef81e937a79fa69afa19cdb98a049f62e
PC1 <umdomby@yandex.ru>
2024-08-31 01:10:53 +0300

    edit

--- GAME-FRONTEND/src/components/modals/EditMyDevice.js
+++ GAME-FRONTEND/src/components/modals/EditMyDevice.js
@@ -91,7 +91,6 @@ const EditMyDevice = observer(({show, onHide}) => {
                                     <input
                                         type="time"
                                         step="0.001"
-                                        pattern="[0-2]{1}[0-9]{1}:[0-5]{1}[0-9]{1}"
                                         defaultValue={deviceMap.timestate}
                                         onChange={e => {
                                              setIdRend({id:deviceMap.id})
