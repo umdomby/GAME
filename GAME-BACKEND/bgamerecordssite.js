42c01134272b644c5383c2317cdeb4ed65821bf4
PC1 <umdomby@yandex.ru>
2024-09-12 19:11:26 +0300

    edit

--- GAME-BACKEND/bgamerecordssite.js
+++ GAME-BACKEND/bgamerecordssite.js
@@ -8,47 +8,22 @@ const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
-
-const fs = require('fs');
-//const http = require('http');
-const https = require('https');
-
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
-
 // Обработка ошибок, последний Middleware
 app.use(errorHandler)
-
-const privateKey = fs.readFileSync(path.resolve('/etc/letsencrypt/live/gamerecords.site/privkey.pem'));
-const certificate = fs.readFileSync(path.resolve('/etc/letsencrypt/live/gamerecords.site/cert.pem'));
-const ca = fs.readFileSync(path.resolve('/etc/letsencrypt/live/gamerecords.site/chain.pem'));
-
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-};
-const httpsServer = https.createServer(credentials, app);
-
-//const httpServer = http.createServer(app);
 
 const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        //app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
-
-        httpsServer.listen(PORT, () => {
-            console.log(`Server started on port ${PORT}`);
-        });
+        app.listen(5001, () => console.log(`Server started on port 5001`))
     } catch (e) {
         console.log(e)
     }
 }
-
 start()
--- GAME-FRONTEND/fgamerecordssite.js
+++ GAME-FRONTEND/fgamerecordssite.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const express = require('express');
-
 const path = require('path')
 
 // const privateKey = fs.readFileSync(path.resolve('/etc/letsencrypt/live/gamerecords.site/privkey.pem'));
@@ -19,12 +18,10 @@ const app = express()
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
-
 const httpServer = http.createServer(app);
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-
 httpServer.listen(8080, () => {
     console.log('HTTP Server running on port 8080');
 });
