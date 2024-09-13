d7e25cde9e8006e1861a7c09a77f8f277bb38b94
PC1 <umdomby@yandex.ru>
2024-09-13 05:49:13 +0300

    edit

--- GAME-BACKEND/bgamerecordssite.js
+++ GAME-BACKEND/bgamerecordssite.js
@@ -7,7 +7,7 @@ const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
-
+const fs = require('fs');
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -17,11 +17,26 @@ app.use('/api', router)
 // Обработка ошибок, последний Middleware
 app.use(errorHandler)
 
+const https = require('https');
+const privateKey = fs.readFileSync(path.resolve('/etc/letsencrypt/live/gamerecords.site/privkey.pem'));
+const certificate = fs.readFileSync(path.resolve('/etc/letsencrypt/live/gamerecords.site/cert.pem'));
+const ca = fs.readFileSync(path.resolve('/etc/letsencrypt/live/gamerecords.site/chain.pem'));
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+const httpsServer = https.createServer(credentials, app);
+
+// const http = require('http');
+// const httpServer = http.createServer(app);
 const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(5001, () => console.log(`Server started on port 5001`))
+        httpsServer.listen(5000, () => {
+            console.log(`Server started on port 5000`);
+        });
     } catch (e) {
         console.log(e)
     }
