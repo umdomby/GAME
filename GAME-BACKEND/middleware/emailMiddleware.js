3f90426dc6a7220bc0d73c4d3162d16e07cdb30b
PC1 <umdomby@yandex.ru>
2024-12-02 03:00:38 +0300

    edit

--- GAME-BACKEND/middleware/emailMiddleware.js
+++ GAME-BACKEND/middleware/emailMiddleware.js
@@ -17,7 +17,7 @@ class emailMiddleware{
             next()
         }
         try {
-            const token = req.headers.authorization.split(' ')[1] // Bearer asfasnfkajsfnjk
+            const token = req.headers.authorization.split(' ')[1]
             if (!token) {
                 return res.status(401).json({message: "Не авторизован"})
             }
